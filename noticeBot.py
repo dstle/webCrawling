add25494ede048c9fa5d7eb183c8031e67fb5992
sudo-dustle <asdf748596123@gmail.com>
2025-03-12 22:29:13 +0900

    log: 로그 추가 및 주석 변경

--- noticeBot.py
+++ noticeBot.py
@@ -6,148 +6,131 @@ import win32gui
 import requests
 import json
 import logging
-import requests 
-
 from bs4 import BeautifulSoup
 from operator import eq
 from apscheduler.schedulers.background import BackgroundScheduler
 from logging.handlers import TimedRotatingFileHandler
 
-# # 카톡창 이름, (활성화 상태의 열려있는 창)
+# 카톡창 이름 리스트
 kakao_opentalk_name = ['noticebot', 'noticebot2']
 idx = 0
 
+# 로거 설정
+def set_logger():
+    global botLogger
+    botLogger = logging.getLogger("KakaoBot")
+    botLogger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+    
+    rotatingHandler = TimedRotatingFileHandler(
+        filename='./noticebot_log/webCrawling.log', when='midnight', encoding='utf-8', backupCount=7)
+    rotatingHandler.setLevel(logging.DEBUG)
+    rotatingHandler.setFormatter(formatter)
+    rotatingHandler.suffix = "%Y-%m-%d"
+    
+    botLogger.addHandler(rotatingHandler)
+    botLogger.info("Logger initialized.")
 
-# # 채팅방에 메시지 전송
-def kakao_sendtext(chatroom_name, noticeLists):
-    # # 핸들 _ 채팅방
-    hwndMain = win32gui.FindWindow(None, chatroom_name)
-    hwndEdit = win32gui.FindWindowEx(hwndMain, None, "RICHEDIT50W", None)
+# 채팅방 열기
+def open_chatroom(chatroom_name):
+    botLogger.info(f"[open_chatroom] Trying to open chatroom: {chatroom_name}")
+    hwnd_kakao = win32gui.FindWindow(None, "카카오톡")
+    hwnd_edit1 = win32gui.FindWindowEx(hwnd_kakao, None, "EVA_ChildWindow", None)
+    hwnd_edit2_1 = win32gui.FindWindowEx(hwnd_edit1, None, "EVA_Window", None)
+    hwnd_edit2_2 = win32gui.FindWindowEx(hwnd_edit1, hwnd_edit2_1, "EVA_Window", None)
+    hwnd_edit3 = win32gui.FindWindowEx(hwnd_edit2_2, None, "Edit", None)
 
-    check = len(noticeLists)
+    if hwnd_edit3 == 0:
+        botLogger.error(f"[open_chatroom] Failed to find chatroom search box.")
+        return False
+    
+    win32api.SendMessage(hwnd_edit3, win32con.WM_SETTEXT, 0, chatroom_name)
+    time.sleep(1)
+    SendReturn(hwnd_edit3)
+    time.sleep(1)
+    botLogger.info(f"[open_chatroom] Chatroom '{chatroom_name}' opened.")
+    return True
 
-  
-    for noticeList in noticeLists:
-        win32api.SendMessage(hwndEdit, win32con.WM_SETTEXT, 0, noticeList)
-        SendReturn(hwndEdit)
-        botLogger = logging.getLogger()
-        botLogger.debug(noticeList)
-        time.sleep(3)
-
-
-
-# # 엔터
+# 엔터 입력
 def SendReturn(hwnd):
     win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
     time.sleep(0.01)
     win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
+# 채팅방에 메시지 전송
+def kakao_sendtext(chatroom_name, noticeLists):
+    botLogger.info(f"[kakao_sendtext] Sending {len(noticeLists)} messages to '{chatroom_name}'")
+    hwndMain = win32gui.FindWindow(None, chatroom_name)
+    hwndEdit = win32gui.FindWindowEx(hwndMain, None, "RICHEDIT50W", None)
+    
+    if hwndEdit == 0:
+        botLogger.error(f"[kakao_sendtext] Failed to find chat input box for '{chatroom_name}'")
+        return
+    
+    for notice in noticeLists:
+        message = f"📢 [공지사항] {notice['date']}\n🔹 제목: {notice['title']}\n🔗 링크: {notice['link']}"
+        win32api.SendMessage(hwndEdit, win32con.WM_SETTEXT, 0, message)
+        SendReturn(hwndEdit)
+        botLogger.info(f"[kakao_sendtext] Message sent: {message}")
+        time.sleep(3)
+    
+    botLogger.info(f"[kakao_sendtext] Completed sending messages to '{chatroom_name}'")
 
-# # 채팅방 열기
-def open_chatroom(chatroom_name):
-    # # # 채팅방 목록 검색하는 Edit (채팅방이 열려있지 않아도 전송 가능하기 위하여)
-    hwndkakao = win32gui.FindWindow(None, "카카오톡")
-    hwndkakao_edit1 = win32gui.FindWindowEx(
-        hwndkakao, None, "EVA_ChildWindow", None)
-    hwndkakao_edit2_1 = win32gui.FindWindowEx(
-        hwndkakao_edit1, None, "EVA_Window", None)
-    hwndkakao_edit2_2 = win32gui.FindWindowEx(
-        hwndkakao_edit1, hwndkakao_edit2_1, "EVA_Window", None)
-    hwndkakao_edit3 = win32gui.FindWindowEx(
-        hwndkakao_edit2_2, None, "Edit", None)
-
-    # # Edit에 검색 _ 입력되어있는 텍스트가 있어도 덮어쓰기됨
-    win32api.SendMessage(
-        hwndkakao_edit3, win32con.WM_SETTEXT, 0, chatroom_name)
-    time.sleep(1)   # 안정성 위해 필요
-    SendReturn(hwndkakao_edit3)
-    time.sleep(1)
-
-# 공지사항 크롤링하기
-
-
+# 공지사항 크롤링
 def get_dwu_notice():
     global idx
     url = 'https://www.dongduk.ac.kr/www/contents/kor-noti.do?gotoMenuNo=kor-noti'  
-    response = requests.get(url)    
+    response = requests.get(url)
     dongduk_url = 'https://www.dongduk.ac.kr/www/contents/kor-noti.do?schM=view&page=1&viewCount=10&id='
+    
+    if response.status_code != 200:
+        botLogger.error(f"[get_dwu_notice] Failed to fetch notices. HTTP {response.status_code}")
+        return []
+    
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    notices = soup.select_one('ul.board-basic')
+    elements = notices.select('li > dl')
 
-    if response.status_code == 200:
-        html = response.text
-        soup = BeautifulSoup(html, 'html.parser')
-        notices = soup.select_one('ul.board-basic')
-        elements = notices.select('li > dl')
+    notice_set = set()
+    for element in elements:
+        id = int(element.a.get('onclick').split("'")[1])
+        title = element.a.text.strip()
+        date = element.find_all('span', 'p_hide')[1].text
+        notice_set.add({"id": id, "title": title, "date": date, "link": f"{dongduk_url}{id}"})
 
-        notice_set = set()
+    new_notices = [el for el in notice_set if el["id"] > idx]
+    if new_notices:
+        new_notices.sort(key=lambda x: x["id"])
+        idx = new_notices[-1]["id"]
+        botLogger.info(f"[get_dwu_notice] {len(new_notices)} new notices fetched.")
+        return new_notices
+    
+    botLogger.info("[get_dwu_notice] No new notices found.")
+    return []
 
-        for element in elements:
-            id = element.a.get('onclick').split("'")[1] # onclick 속성 값 중 " ' " 로 split 해서 두번째 값 가져옴
-            title = element.a.text.strip() 
-            date = element.find_all('span', 'p_hide')[1].text # .find_all(태그 이름, 속성) 해당 하는 정보 모두 조회
-            
-            notice_set.add((int(id), title, date))
-
-        list = [element for element in notice_set if element[0] > idx]
-
-        list.sort(key = lambda x:x[0])
-        idx = list[-1][0]
-
-        noticeList = []
-        for element in list:
-            notice = '[' + element[2] + ']\n' + element[1] + '\n' + dongduk_url + str(element[0])
-            noticeList.append(notice)
-
-        return noticeList
-
-
-# # 스케줄러 job : 매 시간마다 공지사항 크롤링해서 가져오기
+# 스케줄러 job
 def job():
-    p_time_ymd_hms = \
-        f"{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday} / " \
-        f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}"
-
+    botLogger.info("[job] Running scheduled job...")
     noticeList = get_dwu_notice()
     
     for chatroom in kakao_opentalk_name: 
-        # 채팅방 2개 모두 오픈
-        open_chatroom(chatroom)
-        # 메시지 전송, time/실검
-        kakao_sendtext(chatroom, noticeList)
+        if open_chatroom(chatroom):
+            kakao_sendtext(chatroom, noticeList)
+    
+    botLogger.info("[job] Job completed.")
 
-
-# # log 환경설정
-def set_logger():
-    botLogger = logging.getLogger()
-
-    # setting log file level -> DEBUG
-    botLogger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s",
-                                  "%Y-%m-%d %H:%M:%S")
-
-    # 일주일에 한번 월요일 자정에 로그 파일 새로 생성. 최대 5개까지 파일 관리.
-    rotatingHandler = TimedRotatingFileHandler(
-        filename='./noticebot_log/webCrawling.log', when='W0', encoding='utf-8', backupCount=5, atTime=datetime.time(0, 0, 0))
-    rotatingHandler.setLevel(logging.DEBUG)
-    rotatingHandler.setFormatter(formatter)
-
-    # 파일 이름 suffix 설정 (webCrawling.log.yyyy-mm-dd-hh-mm 형식)
-    rotatingHandler.suffix = datetime.datetime.today().strftime("%Y-%m-%d-%H-%M")
-    botLogger.addHandler(rotatingHandler)
-
-        
+# 메인 함수
 def main():
+    set_logger()
+    botLogger.info("Bot is starting...")
     sched = BackgroundScheduler()
     sched.start()
-    set_logger()
-
-    # 15분마다 실행
     sched.add_job(job, 'interval', minutes=15)
-
+    
     while True:
-        botLogger = logging.getLogger()
-        botLogger.debug("-------------실행 중-------------")
+        botLogger.debug("[main] Bot is running...")
         time.sleep(900)
-
 
 if __name__ == '__main__':
     main()
